54f817cdc0c495c51300c87ff8db8c955b008a31
Victor Carnaval <victor.carnaval@seasonsjc.local>
2019-04-01 16:10:02 -0300

    Quiz App developed with HTML, CSS Javascript

---
+++ js/end.js
@@ -0,0 +1,31 @@
+const username = document.getElementById('username');
+const saveScoreBtn = document.getElementById('save-score-btn');
+
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+const finalScore = document.getElementById('final-score');
+
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+const MAX_HIGH_SCORES = 5;
+
+finalScore.innerText = mostRecentScore;
+
+username.addEventListener('keyup', () => {
+    saveScoreBtn.disabled = !username.value;
+});
+
+saveHighScore = e => {
+    e.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    };
+
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('index.html');
+};
